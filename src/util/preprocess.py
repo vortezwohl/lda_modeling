c632d1b17c15bd7caeb08949beb0bbdef6ff1662
吴子豪 <vortez.wohl@gmail.com>
2025-04-12 17:28:47 +0800

    优化 NER

--- src/util/preprocess.py
+++ src/util/preprocess.py
@@ -11,14 +11,50 @@ openai_client = OpenAI(base_url='https://chatnio.cdreader.vip/v1', max_retries=1
 llm = 'qwen-plus'
 
 
-def split_words(text: str, stopwords: list) -> list:
-    known_names = ner(text)
-    for w in stopwords:
-        text = text.replace(w, '_')
-    for n in known_names:
-        text = text.replace(n, '_')
-    seg_list = jieba.cut(text, cut_all=False)
-    return [w for w in seg_list if len(w) > 1 and '_' not in w and not w.isnumeric()]
+def llm_invoke(user_prompt: dict, system_prompt: dict) -> list:
+    while True:
+        try:
+            response = openai_client.chat.completions.create(
+                model=llm,
+                messages=[
+                    {'role': 'user', 'content': json.dumps(user_prompt, ensure_ascii=False)},
+                    {'role': 'system', 'content': json.dumps(system_prompt, ensure_ascii=False)},
+                ],
+                temperature=0.1,
+                top_p=0.2
+            ).choices[0].message.content
+            items = json.loads(response[response.find('['): response.find(']') + 1])
+            return items
+        except json.decoder.JSONDecodeError:
+            ...
+        except openai.InternalServerError:
+            ...
+        except openai.AuthenticationError:
+            print('Authentication error')
+
+
+def check(names: list):
+    text = str(names)
+    role = '通用社会概念识别算法'
+    output_restraint = '你的输出只能在同一行, JSONL格式, 请确保 JSONL 的格式合法.'
+    system_prompt = {
+        '你是': role,
+        '输出数据格式': 'JSONL',
+        '输出示例': ['丈夫', '奶奶', '中年妇女', '警察', '总裁', '皇后', '宝宝'],
+        '输出格式限制': output_restraint
+    }
+    prompt = {
+        '花名册': text,
+        '系统指令设定': system_prompt,
+        '任务目标': '请你仔细逐一检查[花名册], 找出文段中所提及的**所有通用社会概念**, 对于所有词汇, 如果该词汇不是一个**人名**, **地名**, **机构名**等名称, 那么它就是一个通用社会概念.',
+        '限制': '不要提取人名'
+    }
+    non_names = llm_invoke(prompt, system_prompt)
+    print('These arent names:', non_names)
+    for non_name in non_names:
+        if non_name in names:
+            names.remove(non_name)
+    return names
 
 
 def llm_based_ner(text: str, field: str = '网文') -> list:
@@ -41,26 +77,9 @@ def llm_based_ner(text: str, field: str = '网文') -> list:
         '限制': '对于"陈姨", "铭宝", "吴总"这样的称谓, 你只需要提取 "陈", "铭", "吴" 即可, 不需要涉及通用称谓. 对于 "陈家", "谢家" 则提取其家族姓氏 "陈", "谢".\n'
                 '对于"国王", "皇后", "总裁", "妻子", "丈夫", "警察", "中年妇女"等社会概念, 它们并不是人名, 不需要提取.',
     }
-    while True:
-        try:
-            response = openai_client.chat.completions.create(
-                model=llm,
-                messages=[
-                    {'role': 'user', 'content': json.dumps(prompt, ensure_ascii=False)},
-                    {'role': 'system', 'content': json.dumps(system_prompt, ensure_ascii=False)},
-                ],
-                temperature=0.1,
-                top_p=0.2
-            ).choices[0].message.content
-            names = json.loads(response[response.find('['): response.find(']') + 1])
-            print('GPT NER:', names)
-            return names
-        except json.decoder.JSONDecodeError:
-            ...
-        except openai.InternalServerError:
-            ...
-        except openai.AuthenticationError:
-            print('Authentication error')
+    names = llm_invoke(prompt, system_prompt)
+    print('Names Found:', names)
+    return check(names)
 
 
 def ner(text: str) -> list:
@@ -76,3 +95,13 @@ def ner(text: str) -> list:
     for _names in tmp_names:
         names.extend(_names)
     return list(set(names))
+
+
+def split_words(text: str, stopwords: list) -> list:
+    known_names = ner(text)
+    for w in stopwords:
+        text = text.replace(w, '_')
+    for n in known_names:
+        text = text.replace(n, '_')
+    seg_list = jieba.cut(text, cut_all=False)
+    return [w for w in seg_list if len(w) > 1 and '_' not in w and not w.isnumeric()]
