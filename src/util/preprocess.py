a4305bba7c6399c0dc73ef13b86907603fd935d7
吴子豪 <vortez.wohl@gmail.com>
2025-04-12 14:56:39 +0800

    update

--- src/util/preprocess.py
+++ src/util/preprocess.py
@@ -3,13 +3,41 @@ import spacy
 
 ner_model = spacy.load("zh_core_web_sm")
 
+known_names = [
+    "楚辞",
+    "季烟",
+    "津宸",
+    "厉寒",
+    "乐珊",
+    "星晚",
+    "凌远",
+    "鹿溪",
+    "明微",
+    "庭琛",
+    "厉庭琛",
+    "北安",
+    "凉薇",
+    "雨清",
+    "季成德",
+    "澄映",
+    "念安",
+    "青柠",
+    "易辰",
+    "余清舒",
+    "舒念",
+    "南音",
+    "延深",
+    "鹤年",
+    "临州"
+]
+
 
 def split_words(text: str, stopwords: list) -> list:
     stopwords.extend(extract_names(text))
     for w in stopwords:
         text = text.replace(w, '_')
     seg_list = jieba.cut(text, cut_all=False)
-    seg_list = [w for w in seg_list if w not in stopwords and len(w) > 1]
+    seg_list = [w for w in seg_list if w not in stopwords and w not in known_names and len(w) > 1]
     seg_list = [w for w in seg_list if '_' not in w]
     seg_list = [w for w in seg_list if not w.isnumeric()]
     return seg_list
